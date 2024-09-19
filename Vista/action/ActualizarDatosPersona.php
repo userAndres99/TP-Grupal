df858ec782eacfd11b8dbc1bd85a14662d64f74d
unknown <andresmenard.arg99@gmail.com>
2024-09-19 13:06:55 -0300

    modificaciones en el Actulizar persona

--- Vista/action/ActualizarDatosPersona.php
+++ Vista/action/ActualizarDatosPersona.php
@@ -12,15 +12,30 @@ $datos = darDatosSubmitted();
 $personaSeleccionada = convert_array($ABMPersona->buscar(['nrodni' => $datos['nrodni']]))[0];
 $errorOp = 0;
 $modificado = false;
+$sinCambios = true;  // el coso para ver si hay cambios
+
+// comparar los datos y si diosito quiere sale todo bien
+foreach ($datos as $key => $value) {
+    // empezamos a comparar con los datos de la persona seleccionada de la base de datos
+    if (isset($personaSeleccionada[$key]) && $personaSeleccionada[$key] != $value) {
+        $sinCambios = false;
+        break;
+    }
+}
+
 
 if (is_array($datos) && count($datos) > 0) {
     if (is_array($personaSeleccionada) && count($personaSeleccionada) > 0) {
-        if ($ABMPersona->modificacion($datos)) {
+        //si no se modifico nada entonces no vamo por el error 4
+        if ($sinCambios) {
+            $errorOp = 4;  // Los datos no han cambiado
+
+            //o sino seguimo tal cual
+        } elseif ($ABMPersona->modificacion($datos)) {
             $modificado = true;
         } else $errorOp = 3;
     } else $errorOp = 2;
 } else $errorOp = 1;
-
 ?>
 
 <!-- <!DOCTYPE html>
@@ -45,6 +60,9 @@ if (is_array($datos) && count($datos) > 0) {
                 break;
             case 3:
                 echo "ERROR: No pudo completarse la operación de modificar";
+                break;
+            case 4:
+                echo "ERROR: Los datos que intenta modificar son iguales a los que ya se encuentran en el sistema";
                 break;
             default:
                 echo "ÉXITO!";
